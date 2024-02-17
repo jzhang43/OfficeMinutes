ad01b589b457981e0bab94d2f29fbd45f1c5502a
nickbar01234 <nickbar01234@gmail.com>
2024-02-17 15:57:38 -0500

    Fix schema

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,38 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
+import http from "http";
+import socket from "socket.io";
+
+const PORT = process.env.PORT || 8000;
 
 const app = express();
-const port = process.env.PORT || 8000;
+const server = http.createServer();
+const io = new socket.Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
-app.get("/", (req: Request, res: Response) => {
+const users: string[] = [];
+
+app.use(cors());
+app.get("/api", (req: Request, res: Response) => {
   res.send("Hello, TypeScript Express!");
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  users.push(socket.id);
+
+  socket.on("message", ({ message, socketId }) => {
+    console.log(users, socketId);
+    users
+      .filter((id) => socketId !== id)
+      .forEach(() => io.sockets.to(socketId).emit("response", message));
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
